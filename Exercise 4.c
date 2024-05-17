406c5277e72d3f02ac7a985dcf89ac185dbc6294
Mounota Das <168008578+MounotaDas@users.noreply.github.com>
2024-05-17 20:29:05 +0600

    Add files via upload

---
+++ Exercise 4.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+int time = 25;
+
+if(time<15){
+
+    printf("Good Morning");
+}
+ else if(time>20){
+printf("Good Day");
+}
+else{
+
+    printf("Good Evening");
+}
+  return 0;
+}
